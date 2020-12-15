7c829fbe3b713227561b48a458266ce42b39baec
KacperBober <kacperbober112@gmail.com>
2020-12-15 22:12:15 +0100

    added histograms for RGB colors, gray_scale conversion and gray_scale histogram

--- masks_processing.py
+++ masks_processing.py
@@ -1,6 +1,58 @@
-import cv2
-from glob import glob
-
+from os import path
 from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
 
-image = Image.open()
+
+def rgb_histograms(image):
+    """creates arrays of histogram from input image"""
+    r, g, b = image.split()     # splits image into 3 separate images with one color values
+
+    r_histogram = np.histogram(r, bins=256, range=(0, 256))[0]
+    g_histogram = np.histogram(g, bins=256, range=(0, 256))[0]
+    b_histogram = np.histogram(b, bins=256, range=(0, 256))[0]
+
+    bins = range(0, 256)
+
+    return r_histogram, g_histogram, b_histogram, bins
+
+
+def plot_histograms(r, g, b, gray_scale, color_range, fig, axs):
+    """creates figure with 4 histograms R/G/B colors and gray scale"""
+    axs[0].fill(color_range, r, 'r')
+    axs[0].set_title("Czerwony")
+    axs[0].set_ylim(ymin=0)
+
+    axs[1].fill(color_range, g, 'g')
+    axs[1].set_title("Zielony")
+    axs[1].set_ylim(ymin=0)
+
+    axs[2].fill(color_range, b, 'b')
+    axs[2].set_title("Niebieski")
+    axs[2].set_ylim(ymin=0)
+
+    axs[3].plot(color_range, gray_scale)
+    axs[3].set_title("Skala szarosci")
+    axs[3].set_ylim(ymin=0)
+
+    fig.tight_layout()
+
+
+def main():
+    image_path = path.join("images", "zdjęcie.jpg")
+    image = Image.open(image_path)
+    gray_image = image.convert(mode='L')
+
+    r_histogram, g_histogram, b_histogram, bins = rgb_histograms(image)
+    gray_histogram = np.histogram(gray_image, bins=256, range=(0, 256))[0]
+
+    fig, axs = plt.subplots(4)
+    plot_histograms(r_histogram, g_histogram, b_histogram, gray_histogram, bins, fig, axs)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
+
